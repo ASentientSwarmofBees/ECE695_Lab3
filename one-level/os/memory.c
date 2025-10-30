39f07d08b39bf50dc6d07c922d1e53e196180c3c
ASentientSwarmofBees <jasonr9llins@gmail.com>
2025-10-30 18:38:38 -0400

    *pcb->sysStackPtr?

--- one-level/os/memory.c
+++ one-level/os/memory.c
@@ -202,22 +202,22 @@ int MemoryPageFaultHandler(PCB *pcb) {
   //user stack pointer
   //pcb->currentSavedFrame[PROCESS_STACK_USER_STACKPOINTER];
 
-  if(*(pcb->sysStackPtr) >= pcb->currentSavedFrame[PROCESS_STACK_USER_STACKPOINTER] - 8)
+  if(*pcb->sysStackPtr >= pcb->currentSavedFrame[PROCESS_STACK_USER_STACKPOINTER] - 8)
   {
     dbprintf('m', "MemoryPageFaultHandler (%d): sysStackPtr = 0x%x, user stack ptr = 0x%x.\n", GetCurrentPid(), *pcb->sysStackPtr, pcb->currentSavedFrame[PROCESS_STACK_USER_STACKPOINTER]);
     //write a page fault handler which reads the faulting virtual address, figures out its page number, and allocates 
     //a new physical page for that page number.
     //int pageNumber = pcb->sysStackPtr >> 12;
-    dbprintf('m', "MemoryPageFaultHandler (%d): Checking PTE %d", GetCurrentPid(), pcb->sysStackPtr >> 12);
+    dbprintf('m', "MemoryPageFaultHandler (%d): Checking PTE %d", GetCurrentPid(), *pcb->sysStackPtr >> 12);
     if (pcb->pagetable[pcb->sysStaackPtr >> 12] & MEM_PTE_VALID)
     {
-      dbprintf('m', "MemoryPageFaultHandler (%d): PTE %d is already in use?", GetCurrentPid(), pcb->sysStackPtr >> 12);
+      dbprintf('m', "MemoryPageFaultHandler (%d): PTE %d is already in use?", GetCurrentPid(), *pcb->sysStackPtr >> 12);
       return MEM_FAIL;
     }
     else 
     {
-      dbprintf('m', "MemoryPageFaultHandler (%d): Allocating PTE %d", GetCurrentPid(), pcb->sysStackPtr >> 12);
-      pcb->pagetable[pcb->sysStackPtr >> 12] = MemoryAllocPage() << 12 | MEM_PTE_VALID;
+      dbprintf('m', "MemoryPageFaultHandler (%d): Allocating PTE %d", GetCurrentPid(), *pcb->sysStackPtr >> 12);
+      pcb->pagetable[*pcb->sysStackPtr >> 12] = MemoryAllocPage() << 12 | MEM_PTE_VALID;
       pcb->npages++;
       return MEM_SUCCESS;
     }
