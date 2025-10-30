c3c9e8c26380c58ccdafd18eb0d75cb0ede4e08d
ASentientSwarmofBees <jasonr9llins@gmail.com>
2025-10-30 10:38:57 -0400

    aaaa

--- one-level/os/process.c
+++ one-level/os/process.c
@@ -106,7 +106,7 @@ uint32 get_argument(char *string);
 //
 //----------------------------------------------------------------------
 void ProcessModuleInit () {
-  int		i, j;
+  int		i;
 
   dbprintf ('p', "Entering ProcessModuleInit\n");
   AQueueInit (&freepcbs);
@@ -164,10 +164,7 @@ void ProcessModuleInit () {
     pcbs[i].currentSavedFrame = pcbs[i].sysStackPtr;
     dbprintf('z', "ProcessModuleInit (%d), PCB %d: Set currentSavedFrame to sysStackPtr (0x%x).\n", GetCurrentPid(), i, pcbs[i].currentSavedFrame);
 
-    for (j = 0; j < 1000; j++)
-    {
-      dbprintf('z', "currentSavedFrame[%d] = 0x%x\n", j, *(pcbs[i].currentSavedFrame + j))
-    }
+    dbprintf('z', "current saved frame points to %x, with value %x\n", pcbs[i].currentSavedFrame, *pcbs[i].currentSavedFrame);
     
     //-Use currentSavedFrame like an array to set all the register values needed (PROCESS_STACK_PTBASE, 
     //PROCESS_STACK_PTSIZE, PROCESS_STACK_PTBITS, PROCESS_STACK_USER_STACKPOINTER)
