eead97b9cddea64a8f7b4d27bb2a876c73c0874b
ASentientSwarmofBees <jasonr9llins@gmail.com>
2025-11-02 18:00:40 -0500

    does it need to be fork instead of process_create?

--- fork/apps/simple/hello_world/hello_world.c
+++ fork/apps/simple/hello_world/hello_world.c
@@ -3,5 +3,16 @@
 
 void main (int argc, char *argv[])
 {
-  Printf("hello_world (%d): Hello world!\n", getpid());
+  int child_pid;
+
+  Printf("hello_world (%d): this is the main program.\n", getpid());
+  child_pid = fork();
+  if (child_pid != 0)
+  {
+    printf("hello_world (%d): this is the parent process. child_id = %d.\n", getpid(), child_pid);
+  }
+  else
+  {
+    printf("hello_world (%d): this is the child process. child_id = %d.\n", getpid(), child_pid);
+  }
 }
