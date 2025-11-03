1c3f4259a2e1b529859607f7f591ea6d89629a88
ASentientSwarmofBees <jasonr9llins@gmail.com>
2025-11-02 22:06:31 -0500

    hmm

--- fork/os/memory.c
+++ fork/os/memory.c
@@ -374,7 +374,7 @@ void MemoryHandleROPAccess(PCB *pcb) {
 
   //dbprintf('m', "MemoryHandleROPAccess: Fault page: 0x%x (%d)\n", fault_page, fault_page);
 
-  dbprintf('m', "MemoryHandleROPAccess: PTE page %d, Phys page 0x%x, ref count %d\n", fault_pte_page, fault_phys_page, ppageReferenceCounter[fault_phys_page]);
+  dbprintf('m', "MemoryHandleROPAccess: PTE page %d, 0x%x, ref count %d\n", fault_pte_page, (uint32)pcb->pagetable[fault_pte_page], ppageReferenceCounter[fault_phys_page]);
   if(ppageReferenceCounter[fault_phys_page] == 1) {
     //If there is exactly one process using this page, it should be simply marked as read/write. No copying is 
     //necessary.
@@ -389,7 +389,7 @@ void MemoryHandleROPAccess(PCB *pcb) {
     //PTE marked as read/write (i.e. the readonly bit is cleared).
     
     newPage = MemoryAllocPage() << MEM_L1FIELD_FIRST_BITNUM;
-    dbprintf('m', "MemoryHandleROPAccess: Copying from 0x%x to 0x%x (old to new pte)\n", pcb->pagetable[fault_pte_page], newPage);
+    dbprintf('m', "MemoryHandleROPAccess: Copying from 0x%x to 0x%x (old to new pte)\n", (uint32)(pcb->pagetable[fault_pte_page]) & 0x00000FFF, newPage);
     bcopy((uint32)(pcb->pagetable[fault_pte_page]) & 0x00000FFF, newPage, MEM_PAGESIZE);
     pcb->pagetable[fault_pte_page] = ((uint32)pcb->pagetable[fault_pte_page] & 0x00000FFF) | newPage;
     pcb->pagetable[fault_pte_page] &= ~MEM_PTE_READONLY;
