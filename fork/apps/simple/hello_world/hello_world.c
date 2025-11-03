eb41f9255173a97823083134a46e19bc59c6478b
ASentientSwarmofBees <jasonr9llins@gmail.com>
2025-11-02 23:04:23 -0500

    a test

--- fork/apps/simple/hello_world/hello_world.c
+++ fork/apps/simple/hello_world/hello_world.c
@@ -7,7 +7,7 @@ void main (int argc, char *argv[])
 
   Printf("hello_world (%d): this is the main program.\n", (int) getpid());
   child_pid = fork();
-  if (child_pid != 0)
+  if (getpid() != 0)
   {
     Printf("hello_world (%d): this is the parent process. child_id = %d.\n", (int) getpid(), child_pid);
   }
