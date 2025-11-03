fe6eb6cc228bcb3cbf5d9c8e6a1035dab3e60d04
ASentientSwarmofBees <jasonr9llins@gmail.com>
2025-11-02 21:15:23 -0500

    dbg msgs

--- fork/os/process.c
+++ fork/os/process.c
@@ -1187,7 +1187,10 @@ The items that need to be fixed for this lab in DLXOS are:
   dbprintf('p', "ProcessRealFork(%d): Copying system stack contents.\n", GetCurrentPid());
   bcopy((char *)currentPCB->currentSavedFrame[PROCESS_STACK_PTBASE], (char *)newSystemStackPage, MEM_PAGESIZE);
 
-  dbprintf('p', "childPCB currentSavedFrame 0x%x. offset masked 0x%x. newSystemStackPage 0x%x. with offset 0x%x.\n", childPCB->currentSavedFrame, ((childPCB->currentSavedFrame) & 0x00000FFF), newSystemStackPage, (newSystemStackPage | ((childPCB->currentSavedFrame) & 0x00000FFF)));
+  dbprintf('p', "childPCB currentSavedFrame 0x%x.\n", (uint32)childPCB->currentSavedFrame);
+  dbprintf('p', "offset masked 0x%x.\n", (uint32)(childPCB->currentSavedFrame & 0x00000FFF));
+  dbprintf('p', "newSystemStackPage 0x%x.\n", (uint32)newSystemStackPage); 
+  dbprintf('p', "with offset 0x%x.\n", (uint32)((childPCB->currentSavedFrame & 0x00000FFF) | newSystemStackPage));
 
   childPCB->currentSavedFrame = (uint32)((childPCB->currentSavedFrame & 0x00000FFF) | newSystemStackPage);
   dbprintf('p', "ProcessRealFork(%d): updated childPCB currentSavedFrame to 0x%x.\n", GetCurrentPid(), (uint32)childPCB->currentSavedFrame);
